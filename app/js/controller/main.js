8ea47ea12c351f4c27991c71514048f2429c09b5
Garrett Klausner <g@Garretts-MBP.hsd1.va.comcast.net>
2017-04-25 20:48:21 -0400

    Improve UI uniformity.

--- app/js/controller/main.js
+++ app/js/controller/main.js
@@ -1,12 +1,6 @@
 'use strict';
 
 module.exports = function($scope, PatientService, $location) {
-  $scope.patient = PatientService.patient;
-
-  $scope.ResetPatient = PatientService.ResetPatient;
-
-  $scope.Submit = PatientService.Submit;
-
   $scope.IsActive = function (route) {
         return route === $location.path();
   };
